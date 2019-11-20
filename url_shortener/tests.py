428d92f68ad34e6253165efe5cafaff4acccf971
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-20 13:34:09 +0200

    Added tests for basic link creation with valid and invalid data

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+
+class LinkTestCase(TestCase):
+	add_link_url = reverse('link-list')
+
+	def setUp(self) -> None:
+		pass
+
+	def test_link_add_valid(self):
+		default_lifetime = 90
+		data = {
+			'original_url': 'https://ua.jooble.org/'
+		}
+		response = self.client.post(self.add_link_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+		self.assertEquals(response.data['lifetime'], default_lifetime)
+
+	def test_link_add_invalid_lifetime(self):
+		data = {
+			'original_url': 'https://ua.jooble.org/',
+			'lifetime': 0
+		}
+		response = self.client.post(self.add_link_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+		data['lifetime'] = 361
+		response = self.client.post(self.add_link_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
