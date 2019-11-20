8952fbcc3a95c4e0012aaf8d52cd6e7deb732329
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-20 13:22:45 +0200

    Removed check for unique url, because they can be the same but with different lifetimes

--- url_shortener/serializers.py
+++ url_shortener/serializers.py
@@ -5,15 +5,15 @@ from url_shortener.models import Link
 from url_shortener.utils import encode
 
 
-class LinkSerializer(serializers.ModelSerializer):
+class LinkSerializer(serializers.HyperlinkedModelSerializer):
 	short_url = serializers.SerializerMethodField(read_only=True)
 	visits = serializers.IntegerField(read_only=True)
 	created = serializers.DateTimeField(format='%H:%M:%S %d %b %Y', read_only=True)
-	lifetime = serializers.IntegerField(min_value=1, max_value=360)
+	lifetime = serializers.IntegerField(min_value=1, max_value=360, default=90)
 
 	class Meta:
 		model = Link
-		fields = ('short_url', 'original_url', 'visits', 'created', 'lifetime')
+		fields = ('url', 'short_url', 'original_url', 'visits', 'created', 'lifetime')
 
 	def get_short_url(self, obj):
 		url = get_current_site(self.context.get('request')).domain
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
-from rest_framework.response import Response
 
 from url_shortener.models import Link
 from url_shortener.serializers import LinkSerializer
@@ -12,18 +11,6 @@ from url_shortener.utils import decode
 class LinkListCreateApiView(ListCreateAPIView):
 	queryset = Link.objects.all()
 	serializer_class = LinkSerializer
-
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		url = serializer.validated_data['original_url']
-		links = Link.objects.filter(original_url=url)
-		if links.exists():
-			serializer = self.get_serializer(links[0])
-			return Response(serializer.data, status=status.HTTP_200_OK)
-		self.perform_create(serializer)
-		headers = self.get_success_headers(serializer.data)
-		return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class LinkDetailApiView(RetrieveAPIView):
