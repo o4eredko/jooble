17e4603d829c0e00011925f5a2f165f484ed99b3
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-20 13:36:17 +0200

    Added test for invalid url

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -18,7 +18,7 @@ class LinkTestCase(TestCase):
 		self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 		self.assertEquals(response.data['lifetime'], default_lifetime)
 
-	def test_link_add_invalid_lifetime(self):
+	def test_link_error_invalid_lifetime(self):
 		data = {
 			'original_url': 'https://ua.jooble.org/',
 			'lifetime': 0
@@ -26,5 +26,15 @@ class LinkTestCase(TestCase):
 		response = self.client.post(self.add_link_url, data=data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 		data['lifetime'] = 361
+		response = self.client.post(self.add_link_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+	def test_link_error_invalid_url(self):
+		data = {
+			'original_url': 'https://'
+		}
+		response = self.client.post(self.add_link_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+		data['original_url'] = 'https://jooble/'
 		response = self.client.post(self.add_link_url, data=data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
