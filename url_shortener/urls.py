83e91e6083cc6b537d2bdb7fbd381ec3e10c0442
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-20 12:16:26 +0200

    Added check for existing url

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -5,5 +5,6 @@ from url_shortener.views import *
 
 urlpatterns = [
 	path('links/', LinkListCreateApiView.as_view(), name='link-list'),
+	path('links/<int:pk>', LinkDetailApiView.as_view(), name='link-detail'),
 	path('<encoded_url>', redirect_view, name='redirect-link')
 ]
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -13,6 +13,18 @@ class LinkListCreateApiView(ListCreateAPIView):
 	queryset = Link.objects.all()
 	serializer_class = LinkSerializer
 
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		url = serializer.validated_data['original_url']
+		links = Link.objects.filter(original_url=url)
+		if links.exists():
+			serializer = self.get_serializer(links[0])
+			return Response(serializer.data, status=status.HTTP_200_OK)
+		self.perform_create(serializer)
+		headers = self.get_success_headers(serializer.data)
+		return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class LinkDetailApiView(RetrieveAPIView):
 	queryset = Link.objects.all()
